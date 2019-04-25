fe6ae3e1ae4a815a4ead4db3b2c9534460e74e49
Reeahn <36976247+Reeahn@users.noreply.github.com>
2019-04-25 21:47:50 +0930

    added first name and last name

--- Updated02 23_4-Wei-Liu-django_project2/users/forms.py
+++ Updated02 23_4-Wei-Liu-django_project2/users/forms.py
@@ -9,7 +9,7 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -24,4 +24,3 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
-
